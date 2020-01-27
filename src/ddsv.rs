7c45a9ad70275ec8dad1f42dee4e063e9e2f067f
akitsu-sanae <akitsu.sanae@gmail.com>
2020-01-28 01:36:14 +0900

    generate lts directly

--- src/ddsv.rs
+++ src/ddsv.rs
@@ -147,9 +147,8 @@ fn calc_transitions_from<T: Clone>(
         }
     }
 }
-use std::fmt::Debug;
 // TODO: don't use String as error type!
-pub fn concurrent_composition<T: Clone + Debug + Hash + Eq>(
+pub fn concurrent_composition<T: Clone + Hash + Eq>(
     vars: T,
     processes: Vec<Process<T>>,
 ) -> Result<Lts<T>, String> {
@@ -174,57 +173,53 @@ pub fn concurrent_composition<T: Clone + Debug + Hash + Eq>(
         }
         return next;
     };
-    let table = bfs(s0, next_func);
-    Ok(conv_lts(table))
-}
-
-fn conv_lts<T: Clone + Hash + Eq>(
-    table: HashMap<State<T>, (usize, Vec<(Label, State<T>)>)>,
-) -> Lts<T> {
-    let mut lts = Lts::new();
-    for (state, (id, nexts)) in table.iter() {
-        let nexts = nexts
-            .iter()
-            .map(|(label, dst)| {
-                let (dst_id, _) = table.get(&dst).unwrap();
-                (label.clone(), *dst_id)
-            })
-            .collect();
-        lts.0.insert(
-            *id,
-            StateEx {
-                state: state.clone(),
-                transs: nexts,
-                mark: Mark::empty(),
-            },
-        );
-    }
-    lts
+    Ok(bfs(s0, next_func))
 }
 
 fn bfs<T: Clone + Hash + Eq>(
     init: State<T>,
     next_func: impl Fn(&State<T>) -> Vec<(Label, State<T>)>,
-) -> HashMap<State<T>, (usize, Vec<(Label, State<T>)>)> {
-    let mut table = HashMap::new();
-    table.insert(init.clone(), (0, vec![]));
+) -> Lts<T> {
+    let mut lts = Lts::new();
+    lts.0.insert(
+        0,
+        StateEx {
+            state: init.clone(),
+            transs: vec![],
+            mark: Mark::empty(),
+        },
+    );
+    let mut state_dict: HashMap<State<T>, usize> = HashMap::new();
+    state_dict.insert(init.clone(), 0);
     let mut queue = VecDeque::new();
-    queue.push_back((init, 0));
+    queue.push_back((0, init));
 
     loop {
-        if let Some((state, id)) = queue.pop_front() {
+        if let Some((state_id, state)) = queue.pop_front() {
             let nexts = next_func(&state);
-            table.insert(state, (id, nexts.clone()));
-            for (_, dst) in nexts {
-                if table.get(&dst).is_none() {
-                    let id = table.len();
-                    table.insert(dst.clone(), (id, vec![]));
-                    queue.push_back((dst.clone(), id));
-                }
+            let mut transs = Vec::with_capacity(nexts.len());
+            for (label, next_state) in nexts {
+                let next_id = if let Some(id) = state_dict.get(&next_state) {
+                    *id // already exists
+                } else {
+                    let id = state_dict.len();
+                    state_dict.insert(next_state.clone(), id);
+                    queue.push_back((id, next_state.clone()));
+                    id
+                };
+                transs.push((label, next_id));
             }
+            lts.0.insert(
+                state_id,
+                StateEx {
+                    state: state,
+                    transs: transs,
+                    mark: Mark::empty(),
+                },
+            );
         } else {
             break;
         }
     }
-    table
+    lts
 }
